a4e15c7b9c6ec823da848c11b9769c04446dd260
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2021-10-12 15:09:02 +0300

    Started the rewrite

--- js/script.js
+++
@@ -1,1 +0,0 @@
-var elements=document.getElementsByClassName("projects");for(var element of elements)element.addEventListener("scroll",function(){disableBorder(element)});var lastKnownPosition=0,scrollBorderChangeAt=98;function disableBorder(e){var t=e.scrollWidth-e.offsetWidth,o=e.scrollLeft/t*100;o<scrollBorderChangeAt||(lastKnownPosition=o)}
